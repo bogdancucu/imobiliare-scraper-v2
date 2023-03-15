0be878ad0be3b94c95acd22189115d2b74c117de
bogdancucu <nicolaebogdan.cucu@gmail.com>
2023-03-15 19:25:51 +0200

    OOP logic implementation.

--- imobiliare_scraper/imobiliare_scraper/sqs_dead_letter_queue.py
+++ imobiliare_scraper/imobiliare_scraper/sqs_dead_letter_queue.py
@@ -1,41 +1,64 @@
-# This script checks SQS Dead Letter Queue every 12 hours for any failed messages.
 import boto3
+from botocore.exceptions import ClientError
 
 import os
 
 from dotenv import load_dotenv
 load_dotenv()
 
-# Get AWS credentials from .env
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
-SQS_DEAD_LETTER_QUEUE = os.getenv('SQS_DEAD_LETTER_QUEUE')
 
-session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_REGION_NAME
-)
+class SQSDeadLetterQueue:
 
-sqs = session.resource('sqs')
+    # Get AWS credentials
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    REGION_NAME = os.getenv('REGION_NAME')
+    SQS_DEAD_LETTER_QUEUE = os.getenv('SQS_DEAD_LETTER_QUEUE')
 
-queue = sqs.Queue(SQS_DEAD_LETTER_QUEUE)
+    def __init__(self):
+        self.session = None
+        self.sqs = None
+        self.queue = None
+        self.message = None
+
+    def _create_aws_session(self):
+        return boto3.Session(
+            aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY,
+            region_name=self.REGION_NAME
+        )
+
+    def _get_message(self):
+        try:
+            self.session = self._create_aws_session()
+            self.sqs = self.session.resource('sqs')
+            self.queue = self.sqs.Queue(self.SQS_DEAD_LETTER_QUEUE)
+
+            return self.queue.receive_messages(
+                MaxNumberOfMessages=1,
+                WaitTimeSeconds=2,
+                VisibilityTimeout=3
+            )
+        except ClientError as error:
+            raise error
+
+    def process_message(self):
+        self.message = self._get_message()
+
+        try:
+            if self.message:
+                pass
+                # TODO: process message
+            else:
+                pass
+                # TODO: update logfile
+        except ClientError as error:
+            raise error
 
 
 def main():
-    # Retrieve message from SQS Queue
-    message = queue.receive_messages(MaxNumberOfMessages=1,
-                                     WaitTimeSeconds=2,
-                                     VisibilityTimeout=3)
-
-    # Process message
-    if message:
-        # Todo: process the message
-        pass
-    else:
-        # Todo: nothing to process. log this somewhere
-        pass
+    queue = SQSDeadLetterQueue()
+    queue.process_message()
 
 
 if __name__ == '__main__':
--- imobiliare_scraper/imobiliare_scraper/sqs_main_queue.py
+++ imobiliare_scraper/imobiliare_scraper/sqs_main_queue.py
@@ -1,4 +1,5 @@
 import boto3
+from botocore.exceptions import ClientError
 
 from scrapy.crawler import CrawlerProcess
 from spiders.imobiliare_spider import ImobiliareSpider
@@ -9,45 +10,68 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-# Get AWS credentials
-AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
-SQS_MAIN_QUEUE = os.getenv('SQS_MAIN_QUEUE')
 
-session = boto3.Session(
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-    region_name=AWS_REGION_NAME
-)
+class SQSMainQueue:
 
-sqs = session.resource('sqs')
+    # Get AWS credentials
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    REGION_NAME = os.getenv('REGION_NAME')
+    SQS_MAIN_QUEUE_NAME = os.getenv('SQS_MAIN_QUEUE')
 
-queue = sqs.Queue(SQS_MAIN_QUEUE)
+    def __init__(self):
+        self.session = None
+        self.sqs = None
+        self.queue = None
+        self.message = None
+        self.process = None
+        self.urls = None
+
+    def _create_aws_session(self):
+        return boto3.Session(
+            aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY,
+            region_name=self.REGION_NAME
+        )
+
+    def _get_message(self):
+        try:
+            self.session = self._create_aws_session()
+            self.sqs = self.session.resource('sqs')
+            self.queue = self.sqs.Queue(self.SQS_MAIN_QUEUE_NAME)
+
+            return self.queue.receive_messages(
+                MaxNumberOfMessages=1,
+                WaitTimeSeconds=2,
+                VisibilityTimeout=3
+            )
+        except ClientError as error:
+            raise error
+
+    def process_message(self):
+        self.message = self._get_message()
+
+        try:
+            if self.message:
+                self.urls = json.loads(self.message[0].body)['urls']
+
+                self.process = CrawlerProcess()
+                self.process.crawl(
+                    crawler_or_spidercls=ImobiliareSpider,
+                    urls=self.urls
+                )
+                self.process.start()
+            else:
+                print('Nothing on the queue')
+        except ClientError as error:
+            raise error
+        else:
+            self.message[0].delete()
 
 
 def main():
-    # Retrieve message from SQS Queue
-    message = queue.receive_messages(MaxNumberOfMessages=1,
-                                     WaitTimeSeconds=2,
-                                     VisibilityTimeout=3)
-
-    # Process message
-    if message:
-        body_dict = json.loads(message[0].body)
-        urls = body_dict['urls']
-
-        # Crawl
-        process = CrawlerProcess()
-        process.crawl(
-            crawler_or_spidercls=ImobiliareSpider,
-            urls=urls
-        )
-        process.start(stop_after_crawl=True)
-        message[0].delete()
-
-    else:
-        print('Nothing on the queue.')
+    queue = SQSMainQueue()
+    queue.process_message()
 
 
 if __name__ == '__main__':
