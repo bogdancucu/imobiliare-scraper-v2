ad5e9ca4924f830492bad888ff4295020c7ea4b6
bogdancucu <nicolaebogdan.cucu@gmail.com>
2023-03-27 22:50:29 +0300

    added logging functionality (to aws cloudwatch). refactored main_queue.py.

--- imobiliare_scraper/imobiliare_scraper/spiders/imobiliare_spider.py
+++ imobiliare_scraper/imobiliare_scraper/spiders/imobiliare_spider.py
@@ -4,6 +4,9 @@ import scrapy
 from scrapy.loader import ItemLoader
 from itemloaders.processors import TakeFirst
 
+import logging
+from cloudwatch import cloudwatch
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,6 +15,22 @@ AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
 S3_BUCKET_PATH = os.getenv('S3_BUCKET_PATH')
+LOG_GROUP = os.getenv('LOG_GROUP')
+LOG_STREAM_SPIDER = os.getenv('LOG_STREAM_SPIDER')
+
+# CloudWatch logging configuration
+logging.basicConfig(
+    level=logging.DEBUG,
+)
+logger = logging.getLogger('imobiliare_spider')
+handler = cloudwatch.CloudwatchHandler(
+    access_id=AWS_ACCESS_KEY_ID,
+    access_key=AWS_SECRET_ACCESS_KEY,
+    region=AWS_REGION_NAME,
+    log_group=LOG_GROUP,
+    log_stream=LOG_STREAM_SPIDER
+)
+logger.addHandler(hdlr=handler)
 
 
 class ImobiliareSpider(scrapy.Spider):
@@ -40,7 +59,12 @@ class ImobiliareSpider(scrapy.Spider):
 
     def start_requests(self):
         for url in self.urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(
+                url=url,
+                callback=self.parse,
+                errback=self.log_errors,
+                cb_kwargs=dict(url=url)
+            )
 
     def parse(self, response, **kwargs):
         scraped_response = dict()
@@ -48,13 +72,19 @@ class ImobiliareSpider(scrapy.Spider):
 
         item.default_output_processor = TakeFirst()
 
-        item.add_css(field_name='uid', css='span.id::text')
+        try:
+            item.add_css(field_name='uid', css='span.id::text')
+            for spec_name, spec_value in list(zip(
+                response.css('li.list-group-item.specificatii-oferta__lista--item::text').getall(),
+                response.css('li.list-group-item.specificatii-oferta__lista--item span::text').getall(),
+                strict=True
+            )):
+                item.add_value(field_name=spec_name, value=spec_value)
+        except Exception as error:
+            logger.error(error)
+            raise error
+        else:
+            return item.load_item()
 
-        for spec_name, spec_value in list(zip(
-            response.css('li.list-group-item.specificatii-oferta__lista--item::text').getall(),
-            response.css('li.list-group-item.specificatii-oferta__lista--item span::text').getall(),
-            strict=True
-        )):
-            item.add_value(field_name=spec_name, value=spec_value)
-
-        return item.load_item()
+    def log_errors(self, failure):
+        logger.error(f'{repr(failure)}. url: {failure.request.cb_kwargs["url"]}')
--- sqs/main_queue.py
+++ sqs/main_queue.py
@@ -4,48 +4,69 @@ from botocore.exceptions import ClientError
 from scrapy.crawler import CrawlerProcess
 from imobiliare_scraper.imobiliare_scraper.spiders.imobiliare_spider import ImobiliareSpider
 
+import logging
+from cloudwatch import cloudwatch
+
 import json
 import os
 
 from dotenv import load_dotenv
 load_dotenv()
 
+# get AWS credentials from .env file
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+REGION_NAME = os.getenv('REGION_NAME')
+SQS_MAIN_QUEUE = os.getenv('SQS_MAIN_QUEUE')
+LOG_GROUP = os.getenv('LOG_GROUP')
+LOG_STREAM = os.getenv('LOG_STREAM_SQS')
+
+# Configure logger
+logging.basicConfig(
+    level=logging.DEBUG,
+)
+logger = logging.getLogger('sqs_main_queue')
+handler = cloudwatch.CloudwatchHandler(
+    access_id=AWS_ACCESS_KEY_ID,
+    access_key=AWS_SECRET_ACCESS_KEY,
+    region=REGION_NAME,
+    log_group=LOG_GROUP,
+    log_stream=LOG_STREAM
+)
+logger.addHandler(hdlr=handler)
+
 
 class SQSMainQueue:
 
-    # Get AWS credentials
-    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
-    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-    REGION_NAME = os.getenv('REGION_NAME')
-    SQS_MAIN_QUEUE_NAME = os.getenv('SQS_MAIN_QUEUE')
-
-    def __init__(self):
-        self.session = None
-        self.sqs = None
+    def __init__(self, aws_access_key_id, aws_secret_access_key, region_name, sqs_main_queue):
+        self.aws_access_key_id = aws_access_key_id
+        self.aws_secret_access_key = aws_secret_access_key
+        self.region_name = region_name
+        self.sqs_main_queue = sqs_main_queue
         self.queue = None
         self.message = None
         self.process = None
         self.urls = None
 
-    def _create_aws_session(self):
+    def _create_aws_queue_resource(self):
         return boto3.Session(
-            aws_access_key_id=self.AWS_ACCESS_KEY_ID,
-            aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY,
-            region_name=self.REGION_NAME
-        )
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            region_name=self.region_name
+        ).resource('sqs').Queue(self.sqs_main_queue)
 
     def _get_message(self):
         try:
-            self.session = self._create_aws_session()
-            self.sqs = self.session.resource('sqs')
-            self.queue = self.sqs.Queue(self.SQS_MAIN_QUEUE_NAME)
-
+            logger.info('Creating AWS resources')
+            self.queue = self._create_aws_queue_resource()
+            logger.info('Pooling for messages')
             return self.queue.receive_messages(
                 MaxNumberOfMessages=1,
                 WaitTimeSeconds=2,
                 VisibilityTimeout=3
             )
         except ClientError as error:
+            logger.error(f'{error}')
             raise error
 
     def process_message(self):
@@ -53,8 +74,10 @@ class SQSMainQueue:
 
         try:
             if self.message:
+                logger.info('Message on queue. Retrieving for further processing')
                 self.urls = json.loads(self.message[0].body)['urls']
 
+                logger.info(f'Starting the crawl for {len(self.urls)} urls: {self.urls}')
                 self.process = CrawlerProcess()
                 self.process.crawl(
                     crawler_or_spidercls=ImobiliareSpider,
@@ -63,13 +86,21 @@ class SQSMainQueue:
                 self.process.start()
                 self.message[0].delete()
             else:
-                print('Nothing on the queue')
+                logger.info('Nothing on the queue')
         except ClientError as error:
+            logger.error(f'{error}')
             raise error
+        else:
+            logger.info('Crawl finished')
 
 
 def main():
-    queue = SQSMainQueue()
+    queue = SQSMainQueue(
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+        region_name=REGION_NAME,
+        sqs_main_queue=SQS_MAIN_QUEUE
+    )
     queue.process_message()
 
 
